e4cb8a7dc273ebe56919623937bde75048d987c5
Inoxevious <mpasiinnocent@gmail.com>
2021-02-15 10:35:08 +0200

    first commit

--- frontend/react_app/src/components/Footer.js
+++ frontend/react_app/src/components/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
         <Typography variant="body2" color="textSecondary" align="center">
             {'Copyright © '}
             <Link color="inherit" href="https://material-ui.com/">
-                Iris Species Predictor
+                I S Predictor
         </Link>{' '}
             {new Date().getFullYear()}
             {'.'}
